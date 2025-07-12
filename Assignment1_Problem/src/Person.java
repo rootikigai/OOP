e021acb4c68db86bf269ef62b7c77b08400335a4
rootikigai <ifykel360@gmail.com>
2025-07-12 03:14:59 +0100

    Person class added to make a test pass

--- Assignment1_Problem/src/Person.java
+++ Assignment1_Problem/src/Person.java
@@ -1,2 +1,10 @@
-public class Person {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person{
+    private List<Problem> problems = new ArrayList<>();
+
+    public List<Problem> getProblems(){
+        return problems;
+    }
 }
