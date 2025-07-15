1d048faca1abec52e1054e7a375ce6d176cc4772
rootikigai <ifykel360@gmail.com>
2025-07-15 23:20:04 +0100

    Minor update...Added pin authorisation.

--- Assignment2_BankApp/src/BankAccount.java
+++ Assignment2_BankApp/src/BankAccount.java
@@ -1,12 +1,14 @@
-public class BankAccount {
+public class BankAccount{
     private String name;
     private String number;
     private double balance;
+    private String pin;
 
-    public BankAccount(String name, String number, double balance) {
+    public BankAccount(String name, String number, double balance,  String pin) {
         this.name = name;
         this.number = number;
         this.balance = balance;
+        this.pin = pin;
     }
 
     public String getName() {
@@ -21,15 +23,19 @@ public class BankAccount {
         return balance;
     }
 
-    public void deposit(double amt) {
-        if(amt > 0){
-            balance = balance + amt;
+    public void deposit(double amt, String pin){
+        if(this.pin.equals(pin)) {
+            if (amt > 0) {
+                balance = balance + amt;
+            }
         }
     }
 
-    public void withdraw(double amt) {
-        if(amt <= balance){
-            balance = balance - amt;
+    public void withdraw(double amt, String pin){
+        if(this.pin.equals(pin)) {
+            if (amt <= balance) {
+                balance = balance - amt;
+            }
         }
     }
 }
