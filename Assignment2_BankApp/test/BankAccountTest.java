248a033862ad5e7604ecd9dc3540fba25cbdfa3e
rootikigai <ifykel360@gmail.com>
2025-07-15 23:20:28 +0100

    Minor update...Added pin authorisation.

--- Assignment2_BankApp/test/BankAccountTest.java
+++ Assignment2_BankApp/test/BankAccountTest.java
@@ -6,8 +6,18 @@ public class BankAccountTest {
     BankAccount account;
     @BeforeEach
     public void setUp(){
-        account = new BankAccount("Kelvin", "0449352277", 5000.0);
+        account = new BankAccount("Kelvin", "0449352277", 5000.0, "1234");
     }
+
+    @Test
+    public void testThatUserCanCreateAccount(){
+        assertEquals("Kelvin", account.getName());
+        assertEquals("0449352277", account.getNumber());
+        assertEquals(5000.0, account.getBalance());
+        account.deposit(1000.0, "1234");
+        assertEquals(6000.0, account.getBalance());
+    }
+
     @Test
     public void testThatAccountBalance_IsNotEmpty(){
         assertTrue(account.getBalance() > 0);
@@ -15,39 +25,39 @@ public class BankAccountTest {
 
     @Test
     public void tesThatUserCanDeposit_AndBalanceIncreases(){
-        account.deposit(1000);
+        account.deposit(1000, "1234");
         assertEquals(6000, account.getBalance());
-        account.deposit(2000);
+        account.deposit(2000, "1234");
         assertEquals(8000, account.getBalance());
     }
 
     @Test
     public void testThatUserCanWithdraw_AndBalanceDecreases(){
-        account.withdraw(3000.0);
+        account.withdraw(3000.0, "1234");
         assertEquals(2000.0, account.getBalance());
     }
 
     @Test
     public void testThatAccountBalance_IsEmpty_AfterCompleteWithdrawal(){
-        account.withdraw(5000.0);
+        account.withdraw(5000.0, "1234");
         assertEquals(0.0, account.getBalance());
     }
 
     @Test
     public void testThatUserCannotWithdrawAboveBalance(){
-        account.withdraw(7000);
+        account.withdraw(7000, "1234");
         assertEquals(5000.0, account.getBalance());
     }
 
     @Test
     public void testThatDepositMoneyReflectsInAccount(){
-        account.deposit(1000);
+        account.deposit(1000, "1234");
         assertEquals(6000, account.getBalance());
     }
 
     @Test
     public void testThatUserCannotDepositNegativeAmount(){
-        account.deposit(-5000);
+        account.deposit(-5000, "1234");
         assertEquals(5000, account.getBalance());
     }
 }
