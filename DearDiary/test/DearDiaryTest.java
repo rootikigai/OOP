1ea48701c62e5c1e2f5d992cd295ad84d2cb6c88
rootikigai <ifykel360@gmail.com>
2025-07-19 23:09:32 +0100

    Test that diary can be locked and unlocked as well as adding entries. Then I set up the @BeforeEach

--- DearDiary/test/DearDiaryTest.java
+++ DearDiary/test/DearDiaryTest.java
@@ -1,14 +1,34 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DearDiaryTest{
+    @BeforeEach
+    public void setUp(){
+        DearDiary diary = new DearDiary("Kelvin", "fakeasspassword1234");
+    }
     @Test
     public void testThatDiaryCanBeCreated(){
         DearDiary diary = new DearDiary("Kelvin", "fakeasspassword1234");
-        String username = diary.getUserName();
+        String username = diary.getUsername();
         String password = diary.getPassword();
         assertEquals("Kelvin", username);
         assertEquals("fakeasspassword1234", password);
     }
+
+    @Test
+    public void testThatDiaryCanBeLockedAndUnlocked(){
+        DearDiary diary = new DearDiary("Kelvin", "fakeasspassword1234");
+        assertTrue(diary.isLocked());
+        diary.unlockDiary("fakeasspassword1234");
+        assertFalse(diary.isLocked());
+        diary.lockDiary();
+        assertTrue(diary.isLocked());
+    }
+    @Test
+    public void testThatEntryCanBeMadeIntoDiary(){
+        DearDiary diary = new DearDiary("Kelvin", "fakeasspassword1234");
+
+    }
+
 }
