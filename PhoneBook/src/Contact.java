457472fd6dd324e3be55e5699cbe03de70c6c4e3
rootikigai <ifykel360@gmail.com>
2025-07-20 19:58:09 +0100

    UPDATE: Building a phonebook app with OOP using the TDD approach. Wrote enough code to pass the test for invalid or empty first name to throw an exception.

--- PhoneBook/src/Contact.java
+++ PhoneBook/src/Contact.java
@@ -7,6 +7,13 @@ public class Contact {
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
+
+        if(firstName == null || firstName.trim().isEmpty()){
+            throw new IllegalArgumentException("First name cannot be empty");
+        }
+        if(phoneNumber == null || !phoneNumber.matches("\\d{11}")){
+            throw new IllegalArgumentException("Phone number must be 11 digits");
+        }
     }
     public String getFirstName() {
         return firstName;
