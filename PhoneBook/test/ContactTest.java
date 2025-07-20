7c09ea781bd89fb4d068a74eddbd6e1af39e9369
rootikigai <ifykel360@gmail.com>
2025-07-20 19:56:47 +0100

    UPDATE: Building a phonebook app with OOP using the TDD approach. Wrote test for invalid or empty first name to throw an exception.

--- PhoneBook/test/ContactTest.java
+++ PhoneBook/test/ContactTest.java
@@ -1,6 +1,8 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /*
 A phone has a phonebook (I go just skip the phone class las las.)
@@ -11,11 +13,21 @@ A person can delete contact by searching for name or number and deleting
 A person can view all contact
 */
 public class ContactTest {
+    @BeforeEach
+    public void setUp(){
+        Contact contact = new Contact("Kelvin", "Ifeanyi", "08035859351");
+    }
     @Test
     public void testThatContactCanBeCreated() {
         Contact contact = new Contact("Kelvin", "Ifeanyi", "08035859351");
         assertEquals("Kelvin", contact.getFirstName());
         assertEquals("Ifeanyi", contact.getLastName());
         assertEquals("08035859351", contact.getPhoneNumber());
+    }
+    @Test
+    public void testThatInvalidFirstNameThrowsIllegalArgumentException(){
+        assertThrows(IllegalArgumentException.class, ()-> {
+            new Contact("", "Ifeanyi", "08035859351");
+        });
     }
 }
