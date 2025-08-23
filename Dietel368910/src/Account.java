f4aa5ced0bb2fd08b5b97bd3dd59aa3802d55d48
rootikigai <ifykel360@gmail.com>
2025-08-23 23:45:31 +0100

    Account class: Initializing objects with constructors

--- Dietel368910/src/Account.java
+++ Dietel368910/src/Account.java
@@ -1,6 +1,10 @@
 public class Account {
     private String name;
 
+    public Account(String name) {
+        this.name = name;
+    }
+
     public void setName(String name){
         this.name = name;
     }
