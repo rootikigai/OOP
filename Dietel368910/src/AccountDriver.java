ae771bb251bc1d98ed14f99051c0633459e2c5f2
rootikigai <ifykel360@gmail.com>
2025-08-23 23:46:49 +0100

    Initializing account objects at creation

--- Dietel368910/src/AccountDriver.java
+++ Dietel368910/src/AccountDriver.java
@@ -1,14 +1,12 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class AccountDriver {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        Account myAccount = new Account();
-        System.out.printf("Initial name is: %s%n%n", myAccount.getName());
-        System.out.println("Please enter account name:");
-        String accountName = input.nextLine();
-        myAccount.setName(accountName);
-        System.out.println();
-        System.out.printf("Name in object myAccount is:%n%s%n", myAccount.getName());
+//        Scanner input = new Scanner(System.in);
+        Account myAccount1 = new Account("Kelvin Ifeanyi");
+        Account myAccount2 = new Account("Glory Lawrence-Ifeanyi");
+
+        System.out.printf("Name for Account1 is: %s\n", myAccount1.getName());
+        System.out.printf("Name for Account2 is: %s\n", myAccount2.getName());
     }
 }
