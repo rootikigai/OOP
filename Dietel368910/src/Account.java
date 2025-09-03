f681099df5c4c3e345d4e957cfa096462581aa3b
rootikigai <ifykel360@gmail.com>
2025-09-03 23:19:57 +0100

    Added the deposit method

--- Dietel368910/src/Account.java
+++ Dietel368910/src/Account.java
@@ -1,5 +1,6 @@
 public class Account {
     private String name;
+    private double balance;
 
     public Account(String name) {
         this.name = name;
@@ -11,5 +12,14 @@ public class Account {
 
     public String getName(){
         return name;
+    }
+
+    public void deposit(double depositAmount) {
+        if (depositAmount < 0.00) {
+            System.out.println("deposit amount is negative");
+        }
+        else if (depositAmount > 0.00) {
+            balance = balance + depositAmount;
+        }
     }
 }
